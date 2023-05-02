d81bbd042258e6699bbdad3376555e95af704970
Guillaume Fuchs <guillaume.fuchs@gmail.com>
2023-05-02 22:57:20 +0200

    minor update

--- pyrunpace/segment.py
+++ pyrunpace/segment.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-"""Class segment, for a running segment defined by time and distance.
+"""Class segment, for a running segment defined by time and distance."""
 
-
-"""
 
 from pyrunpace import time
 
@@ -24,22 +22,22 @@ class Segment:
         seconds : int, optional
             number of seconds, by default 0
         """
-        self.__distance = float(distance)
-        self.__time = int(3600.0 * hours + 60.0 * minutes + seconds)
-        """compute integer HMS"""
+        self.distance = float(distance)
+        self.time = int(3600.0 * hours + 60.0 * minutes + seconds)
+        # compute integer HMS
         self.__hours = int(hours)
         self.__minutes = int((hours - int(hours)) * 60.0) + int(minutes)
         self.__seconds = int(seconds + (minutes - int(minutes)) * 60.0)
-        """compute modulo 60 HMS"""
+        # compute modulo 60 HMS
         tmp = self.__seconds
         self.__seconds = self.__seconds % 60
         self.__minutes += int(tmp / 60)
         tmp = self.__minutes
         self.__minutes = self.__minutes % 60
         self.__hours += int(tmp / 60)
-        """pace in seconds per km"""
-        self.__pace = self.__time / self.__distance
-        """compute modulo 60 HMS"""
+        # pace in seconds per km
+        self.__pace = self.time / self.distance
+        # compute modulo 60 HMS
         self.__pace_hours = int(self.__pace / 3600)
         self.__pace_seconds = self.__pace - 3600 * self.__pace_hours
         self.__pace_minutes = int(self.__pace_seconds / 60)
@@ -59,37 +57,43 @@ class Segment:
         """
         self.__dict__[name] = value
 
-    def __add__(self, other):
+    def __add__(self, other) -> "Segment":
         if isinstance(other, Segment):
             return self.__class__(
-                self.__distance + other.__distance, self.__time + other.__time
+                self.distance + other.distance, self.time + other.time
             )
-        else:
-            raise TypeError("Illegal argument type for built-in operation")
+
+        raise TypeError("Illegal argument type for built-in operation")
 
     def __str__(self) -> str:
-        return str(self.__distance)
+        """__str__ magic method.
+
+        Returns
+        -------
+        str
+            _description_
+        """
+        return str(self.distance)
 
     def print_info(self) -> None:
         """print_info print information about segment."""
-        print(f"\tDistance: %.2f km {self.__distance}")
+        print(f"\tDistance: {self.distance} km")
         print(f"\tTime: {self.__hours:02d}:{self.__minutes:02d}:{self.__seconds:02d}")
         print(
-            "\tPace: %02d:%02d:%02d/km"
-            % (self.__pace_hours, self.__pace_minutes, self.__pace_seconds)
+            f"\tPace: {self.__pace_hours:02d}{self.__pace_minutes:02d}{self.__pace_seconds:02d}/km"
         )
         print(f"\tSpeed: {self.__speed_kmh:.2f} km/h")
 
         print("Time for usual distances")
         print("========================")
-        h, m, s = time.seconds_to_hms(self.__pace * 42.2)
-        print(f"\tTime Marathon: {h:02d}:{m:02d}:{s:02d}")
+        hours, minutes, seconds = time.seconds_to_hms(self.__pace * 42.2)
+        print(f"\tTime Marathon: {hours:02d}:{minutes:02d}:{seconds:02d}")
 
-        h, m, s = time.seconds_to_hms(self.__pace * 21.1)
-        print(f"\tTime Half-Marathon: {h:02d}:{m:02d}:{s:02d}")
+        hours, minutes, seconds = time.seconds_to_hms(self.__pace * 21.1)
+        print(f"\tTime Half-Marathon: {hours:02d}:{minutes:02d}:{seconds:02d}")
 
-        h, m, s = time.seconds_to_hms(self.__pace * 10)
-        print(f"\tTime 10K: {h:02d}:{m:02d}:{s:02d}")
+        hours, minutes, seconds = time.seconds_to_hms(self.__pace * 10)
+        print(f"\tTime 10K: {hours:02d}:{minutes:02d}:{seconds:02d}")
 
-        h, m, s = time.seconds_to_hms(self.__pace * 5)
-        print(f"\tTime 5K: {h:02d}:{m:02d}:{s:02d}")
+        hours, minutes, seconds = time.seconds_to_hms(self.__pace * 5)
+        print(f"\tTime 5K: {hours:02d}:{minutes:02d}:{seconds:02d}")
--- pyrunpace/time.py
+++ pyrunpace/time.py
@@ -1,4 +1,20 @@
-def seconds_to_hms(time_s) -> tuple:
+"""time module.
+"""
+
+
+def seconds_to_hms(time_s: float) -> tuple:
+    """seconds_to_hms converts time in second to hours:minutes:seconds format.
+
+    Parameters
+    ----------
+    time_s : float
+        time in seconds
+
+    Returns
+    -------
+    tuple
+        hours, miniutes, seconds format
+    """
     hours = int(time_s / 3600)
     seconds = int(time_s) - 3600 * hours
     minutes = int(seconds / 60)
